75bbac7ed62ae6131fd6bbc86f95dea675c7ba4c
abdoukader <aktmere@gmail.com>
2023-04-09 06:49:47 +0200

    Premier commit du Ebanking-backend

---
+++ ebanking-backend/src/main/java/org/sid/ebankingbackend/services/BankService.java
@@ -0,0 +1,38 @@
+package org.sid.ebankingbackend.services;
+
+import org.sid.ebankingbackend.entities.BankAccount;
+import org.sid.ebankingbackend.entities.CurrentAccount;
+import org.sid.ebankingbackend.entities.SavingAccount;
+import org.sid.ebankingbackend.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class BankService {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    public void consulter(){
+        BankAccount bankAccount =
+                bankAccountRepository.findById("227f9148-cf29-411e-a038-d32d3a17a645").orElse(null);
+        System.out.println("***********************************");
+        if (bankAccount!= null) {
+            System.out.println(bankAccount.getId());
+            System.out.println(bankAccount.getBalance());
+            System.out.println(bankAccount.getStatus());
+            System.out.println(bankAccount.getCreatedAt());
+            System.out.println(bankAccount.getCustomer().getName());
+            System.out.println(bankAccount.getClass().getSimpleName());
+            if (bankAccount instanceof CurrentAccount) {
+                System.out.println("Over Draf" + ((CurrentAccount) bankAccount).getOverDraft());
+            } else if (bankAccount instanceof SavingAccount) {
+                System.out.println("Rate" + ((SavingAccount) bankAccount).getInterestRate());
+            }
+            bankAccount.getAccountOperations().forEach(op -> {
+                System.out.println(op.getType() + "\t" + op.getOperationDate() + "\t" + op.getAmount());
+            });
+        }
+    }
+}
